e54f79740b41ce292ee102469ac949e7403873bd
pvor <pvor@CIMW7804>
2017-10-31 18:39:27 +0200

    sdl window parameters

--- opengl_tutorial/Display.cpp
+++ opengl_tutorial/Display.cpp
@@ -3,7 +3,14 @@
 Display::Display(int width, int height, const std::string& title)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	//m_window;
+
+	m_window = SDL_CreateWindow(
+		title.c_str(),
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		width,
+		height,
+		SDL_WINDOW_OPENGL);
 }
 
 
