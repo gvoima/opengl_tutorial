45041223b3fb592e78824c730669baf5299287fa
pvor <pvor@CIMW7804>
2017-10-31 19:32:52 +0200

    added 32b and 64b libraries

--- opengl_tutorial/Display.cpp
+++ opengl_tutorial/Display.cpp
@@ -4,6 +4,13 @@ Display::Display(int width, int height, const std::string& title)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8); // channel buffer bits (color depth)
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 	m_window = SDL_CreateWindow(
 		title.c_str(),
 		SDL_WINDOWPOS_CENTERED,
